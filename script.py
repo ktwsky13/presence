18699d945abe8057afcf248f22ae6c58dac2a08f
ktwsky13 <42086717+ktwsky13@users.noreply.github.com>
2023-03-09 17:21:39 +0300

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ RPC = Presence(client_id=client_id)
 RPC.connect()
 
 # Make sure you are using the same name that you used when uploading the image
-RPC.update(large_image="touching", large_text="It's so cool!")
+RPC.update(state="Touching grass", details="It's so cool!", large_image="touching")
 
 while 1:
     time.sleep(15)
